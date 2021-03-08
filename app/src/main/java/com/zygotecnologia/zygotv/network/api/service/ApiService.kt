ab0a17a22fb4d75869d188caab4e2817f98f580a
josediogonc <josediogonc@gmail.com>
2021-03-08 15:25:23 -0300

    adding some configuration to gradle.properties

--- app/src/main/java/com/zygotecnologia/zygotv/network/api/service/ApiService.kt
+++ app/src/main/java/com/zygotecnologia/zygotv/network/api/service/ApiService.kt
@@ -1,5 +1,6 @@
 package com.zygotecnologia.zygotv.network.api.service
 
+import com.zygotecnologia.zygotv.BuildConfig
 import com.zygotecnologia.zygotv.model.genre.GenreResponse
 import com.zygotecnologia.zygotv.model.season.SeasonResponse
 import com.zygotecnologia.zygotv.model.show.ShowDetails
@@ -14,20 +15,20 @@ interface ApiService {
 
     @GET("genre/tv/list")
     suspend fun fetchGenres(@Query("api_key") apiKey: String = TmdbApi.TMDB_API_KEY,
-                            @Query("region") region: String = TmdbApi.TMDB_API_REGION): Response<GenreResponse>
+                            @Query("region") region: String = BuildConfig.TMDB_API_REGION): Response<GenreResponse>
 
     @GET("tv/popular")
     suspend fun fetchPopularShows(@Query("api_key") apiKey: String = TmdbApi.TMDB_API_KEY,
-                                  @Query("region") region: String = TmdbApi.TMDB_API_REGION): Response<ShowResponse>
+                                  @Query("region") region: String = BuildConfig.TMDB_API_REGION): Response<ShowResponse>
 
     @GET("tv/{id}")
     suspend fun fetchShow(@Path("id") id: Int,
                           @Query("api_key") apiKey: String = TmdbApi.TMDB_API_KEY,
-                          @Query("region") region: String = TmdbApi.TMDB_API_REGION): Response<ShowDetails>
+                          @Query("region") region: String = BuildConfig.TMDB_API_REGION): Response<ShowDetails>
 
     @GET("tv/{id}/season/{season_number}")
     suspend fun fetchSeasonDetails(@Path("id") id: Int, @Path("season_number") seasonNumber: Int,
                           @Query("api_key") apiKey: String = TmdbApi.TMDB_API_KEY,
-                          @Query("region") region: String = TmdbApi.TMDB_API_REGION): Response<SeasonResponse>
+                          @Query("region") region: String = BuildConfig.TMDB_API_REGION): Response<SeasonResponse>
 
 }
--- app/src/main/java/com/zygotecnologia/zygotv/utils/ImageUrlBuilder.kt
+++ app/src/main/java/com/zygotecnologia/zygotv/utils/ImageUrlBuilder.kt
@@ -4,20 +4,18 @@ import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.zygotecnologia.zygotv.BuildConfig
 import com.zygotecnologia.zygotv.R
 import com.zygotecnologia.zygotv.network.TmdbApi
 
 object ImageUrlBuilder {
 
-    private const val POSTER_URL = "https://image.tmdb.org/t/p/w154"
-    private const val BACKDROP_URL = "https://image.tmdb.org/t/p/w780"
-
     fun buildPosterUrl(posterPath: String): String {
-        return POSTER_URL + posterPath + "?${TmdbApi.TMDB_API_QUERY}=" + TmdbApi.TMDB_API_KEY
+        return BuildConfig.POSTER_URL + posterPath + "?${BuildConfig.TMDB_API_QUERY}=" + TmdbApi.TMDB_API_KEY
     }
 
     fun buildBackdropUrl(backdropPath: String): String {
-        return BACKDROP_URL + backdropPath + "?${TmdbApi.TMDB_API_QUERY}=" + TmdbApi.TMDB_API_KEY
+        return BuildConfig.BACKDROP_URL + backdropPath + "?${BuildConfig.TMDB_API_QUERY}=" + TmdbApi.TMDB_API_KEY
     }
 
     fun String.loadImage(itemView : View, imageView: ImageView) {
