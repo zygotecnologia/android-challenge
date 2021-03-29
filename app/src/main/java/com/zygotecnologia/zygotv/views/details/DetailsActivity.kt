d77baca2e4c4038138ff623956b6ce885dc1898e
Isaque Hernandes <isaquehernandesfs@gmail.com>
2021-03-29 01:41:31 -0300

    Basic custom toolbar

--- app/src/main/java/com/zygotecnologia/zygotv/views/details/DetailsActivity.kt
+++ app/src/main/java/com/zygotecnologia/zygotv/views/details/DetailsActivity.kt
@@ -3,6 +3,7 @@ package com.zygotecnologia.zygotv.views.details
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.zygotecnologia.zygotv.R
@@ -25,6 +26,9 @@ class DetailsActivity : BaseViewStates() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        val backButton: ImageView = findViewById(R.id.toolbar_back_button)
+        backButton.setOnClickListener { finish() }
 
         this.showId = intent.getIntExtra(DETAILS_SHOW_ID_KEY, -1)
 
