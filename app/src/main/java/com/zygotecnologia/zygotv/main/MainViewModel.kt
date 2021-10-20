78e9e692a1156d65f3a7402b7f42e8e31c99e474
AndreThlckr <andretheilacker@gmail.com>
2021-10-20 19:00:31 -0300

    Created first test

--- app/src/main/java/com/zygotecnologia/zygotv/main/MainViewModel.kt
+++ app/src/main/java/com/zygotecnologia/zygotv/main/MainViewModel.kt
@@ -1,8 +1,13 @@
 package com.zygotecnologia.zygotv.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.zygotecnologia.zygotv.model.Show
 
 class MainViewModel: ViewModel() {
+
+    val shows: LiveData<List<Show>> = MutableLiveData(emptyList())
 
     private fun loadShows() {
 
--- app/src/test/java/com/zygotecnologia/zygotv/main/MainViewModelTest.kt
+++ app/src/test/java/com/zygotecnologia/zygotv/main/MainViewModelTest.kt
@@ -1,8 +1,14 @@
 package com.zygotecnologia.zygotv.main
 
-import org.junit.Assert.*
+import io.kotest.matchers.collections.shouldNotBeEmpty
+import org.junit.Test
 
 class MainViewModelTest {
 
+    private val viewModel = MainViewModel()
 
+    @Test
+    fun `viewModel should load show list`() {
+        viewModel.shows.value.shouldNotBeEmpty()
+    }
 }
