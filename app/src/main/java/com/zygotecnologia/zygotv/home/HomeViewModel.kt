eb6e66f3450f32c1ed27196468c3207fb3e299e1
AndreThlckr <andretheilacker@gmail.com>
2021-10-21 22:10:08 -0300

    Added filter to empty genres and fixed layout preview

--- app/src/main/java/com/zygotecnologia/zygotv/home/HomeViewModel.kt
+++ app/src/main/java/com/zygotecnologia/zygotv/home/HomeViewModel.kt
@@ -3,7 +3,9 @@ package com.zygotecnologia.zygotv.home
 import androidx.lifecycle.*
 import com.zygotecnologia.zygotv.tmdb.domain.GenreWithShows
 import com.zygotecnologia.zygotv.tmdb.domain.TmdbRepository
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 class HomeViewModel(
     private val tmdbRepository: TmdbRepository
@@ -11,6 +13,8 @@ class HomeViewModel(
 
     private val _showsByGenre = flow {
         emit(tmdbRepository.getShowsByGenre())
+    }.map { list ->
+        list.filter { genre -> genre.shows.isEmpty() }
     }
     val showsByGenre: LiveData<List<GenreWithShows>> = _showsByGenre.asLiveData()
 }
