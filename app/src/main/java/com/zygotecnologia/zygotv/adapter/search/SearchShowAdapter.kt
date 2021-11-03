fd19185b40ecaa59020194383c88555af106a118
Guilherme Pereira <guisil_ps@hotmail.com>
2021-11-03 01:54:17 -0300

    Corrige navegacao para tela de detalhes da serie ao pesquisar serie

--- app/src/main/java/com/zygotecnologia/zygotv/adapter/search/SearchShowAdapter.kt
+++ app/src/main/java/com/zygotecnologia/zygotv/adapter/search/SearchShowAdapter.kt
@@ -1,11 +1,15 @@
 package com.zygotecnologia.zygotv.adapter.search
 
+import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.zygotecnologia.zygotv.adapter.main.MainShowAdapter
 import com.zygotecnologia.zygotv.databinding.RowShowItemBinding
 import com.zygotecnologia.zygotv.model.Show
 import com.zygotecnologia.zygotv.utils.setBackEndImage
+import com.zygotecnologia.zygotv.view.description.DescriptionActivity
 
 class SearchShowAdapter(
     private val showList: MutableList<Show>
@@ -33,14 +37,32 @@ class SearchShowAdapter(
     }
 
     class ViewHolder(private val searchShowView: RowShowItemBinding) :
-        RecyclerView.ViewHolder(searchShowView.root) {
+        RecyclerView.ViewHolder(searchShowView.root), View.OnClickListener {
+
+        private var showId: Int = 0
 
         fun bind(show: Show) {
+            showId = show.id ?: 0
+            searchShowView.showItem.setOnClickListener {
+                onClick(it)
+            }
             searchShowView.tvShowTitle.text = show.name
             searchShowView.ivShowPoster.setBackEndImage(
                 itemView.context,
                 show.posterPath ?: ""
             )
         }
+
+        override fun onClick(v: View?) {
+            searchShowView.root.context.apply {
+                startActivity(Intent(this, DescriptionActivity::class.java).apply {
+                    putExtra(SHOW_ID_KEY, showId)
+                })
+            }
+        }
+    }
+
+    companion object {
+        private const val SHOW_ID_KEY = "ShowID"
     }
 }
