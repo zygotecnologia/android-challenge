ffa2d97bb3d2c6056ba536e570a433d8883a1fec
ftoniolo <ftoniolo@braspag.com.br>
2021-11-07 21:49:43 -0300

    constants in buildconfig

--- app/src/main/java/com/zygotecnologia/zygotv/network/TmdbClient.kt
+++ app/src/main/java/com/zygotecnologia/zygotv/network/TmdbClient.kt
@@ -3,9 +3,7 @@ package com.zygotecnologia.zygotv.network
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import com.zygotecnologia.zygotv.network.TmdbApi.Companion.TMDB_API_KEY
-import com.zygotecnologia.zygotv.network.TmdbApi.Companion.TMDB_API_QUERY
-import okhttp3.Interceptor
+import com.zygotecnologia.zygotv.BuildConfig
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -18,7 +16,7 @@ object TmdbClient {
 
     fun getInstance(): TmdbApi {
         val retrofit = Retrofit.Builder()
-            .baseUrl(TmdbApi.TMDB_BASE_URL)
+            .baseUrl(BuildConfig.TMDB_BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create(moshiAdapter))
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .client(OkHttpClient.Builder().build())
